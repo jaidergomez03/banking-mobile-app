29185812415cb8ff5e58cbdab1df3a8227246b92
jaider <jaidergomez20@gmail.com>
2025-11-07 12:48:40 -0500

    app react native

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Here I am gping to develop my app</Text>
+    </View>
+  );
+}
+                                            
